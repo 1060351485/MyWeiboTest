109802db199fb7c8b82e14af7d0421964fca9f01
Apple <1060351485@qq.com>
2016-03-12 23:38:08 +0800

    remove appkey

---
+++ MyWbTest23/AppKey.h
@@ -0,0 +1,15 @@
+//
+//  AppKey.h
+//  MyWbTest23
+//
+//  Created by Apple on 16/3/12.
+//  Copyright © 2016年 JohnHe. All rights reserved.
+//
+
+#ifndef AppKey_h
+#define AppKey_h
+
+#define kAppKey @"2076931958"
+#define kAppSecret @"be06f50c78b2f3b6a2cb63238e14282e"
+
+#endif /* AppKey_h */
